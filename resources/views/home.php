60d86512f6ce0e08deb557fc281d3bdbd23f927b
Waeco <juanmanu6609@gmail.com>
2024-06-13 10:51:40 -0600

    commit de inicio

---
+++ resources/views/home.php
@@ -0,0 +1,266 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>Inicio</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">
+    </script>
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/jquery.dropotron.min.js"></script>
+    <script src="../js/skel.min.js"></script>
+    <script src="../js/skel-layers.min.js"></script>
+    <script src="../js/init.js"></script><noscript>
+        <link rel="stylesheet" href="skel.css<">
+        <link rel="stylesheet" href="../resources/css/style.css">
+    </noscript>
+</head>
+
+<body class="homepage">
+
+    <div id="header">
+        <div class="container">
+
+            <!-- Logo -->
+            <img src="images/logo.jpeg" class="perfilf">
+            <h1><a href="#" id="logo">AMARANT</a></h1>
+            <h3>"Tradición y calidad a tu paladar"</h3>
+            <!-- Nav -->
+            <nav id="nav">
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+
+
+                    <li> <a href="">Central de abastos</a>
+                        <ul>
+                            <li><a href="#"></a></li>
+                            <li><a href="#">Phasellus consequat</a></li>
+                        </ul>
+                    </li>
+                    </li>
+
+
+                    <li> <a href="">La merced</a>
+                        <ul>
+                            <li><a href="#">Lorem ipsum dolor</a></li>
+                            <li><a href="#">Phasellus consequat</a></li>
+                        </ul>
+                    </li>
+                    </li>
+
+
+                    <li> <a href="">Ecatepec</a>
+                        <ul>
+                            <li><a href="#">Lorem ipsum dolor</a></li>
+                            <li><a href="#">Phasellus consequat</a></li>
+                        </ul>
+                    </li>
+                    </li>
+
+
+                    <li><a href="left-sidebar.html">Left Sidebar</a></li>
+                    <li><a href="right-sidebar.html">Right Sidebar</a></li>
+                    <li><a href="no-sidebar.html">No Sidebar</a></li>
+                </ul>
+            </nav><!-- Banner -->
+            <div id="banner">
+
+
+                <div class="container">
+                    <section>
+                        <header class="major">
+                            <h2>Bienvenido a Amarant!</h2>
+                            <span class="byline">Amarant tuvo sus inicios como pequeño negocio familiar en los años 70s
+                                con la elaboración de diversos dulces típicos mexicanos en los que destaco la
+                                palanqueta.
+                                Las técnicas y procedimientos para la elaboración de este dulce tradicional se
+                                perfeccionaron con el tiempo a través de tres generaciones. </span>
+                        </header><a href="inicio.html" class="button alt">Inicia Sesion</a>
+                </div>
+                </section>
+            </div>
+        </div>
+
+    </div>
+    </div>
+
+    <!-- Featured -->
+    <div class="wrapper style2">
+        <section class="container">
+            <header class="major">
+                <h2>Nuestros productos</h2>
+                <span class="byline">Palanquetas Amarant</span>
+            </header>
+            <div class="row no-collapse-1">
+
+
+                <section class="4u"><a href="#" class="image feature"><img src="images/Pepitap.png" alt="" width="368"
+                            height="180"></a>
+                    <p>Palanquetas con pepita 24pz</p>
+                    <p>Ingredientes: Cacahuate, semilla de calabaza (pepita), ajonjolí, amaranto, azúcar de caña y miel
+                        de maíz.</p>
+
+
+                </section>
+                <section class="4u"><a href="#" class="image feature"><img src="images/Almendrap.png" alt="" width="368"
+                            height="180"></a>
+                    <p>Palanquetas con almendra 24pz</p>
+                    <p>Ingredientes: Almendra, Cacahuate, Ajonjolí, Amaranto, Azúcar de caña y Glucosa de maíz.</p>
+
+
+                </section>
+                <section class="4u"><a href="#" class="image feature"><img src="images/Nuezp.png" alt="" width="368"
+                            height="180"></a>
+                    <p>Palanquetas con nuez 24pz</p>
+                    <p>Ingredientes: Nuez, ajonjolí, amaranto, azúcar de caña y miel de maíz.</p>
+                </section>
+        </section>
+
+
+
+
+        <div class="wrapper style2">
+            <section class="container">
+                <header class="major">
+                </header>
+                <div class="row no-collapse-1">
+                    <section class="4u"><a href="#" class="image feature"><img src="images/cacahuate.png" alt=""
+                                width="368" height="180"></a>
+                        <p>Palanquetas con cacahuate 24pz</p>
+                        <p>Ingredientes: Cacahuate, azúcar de caña y miel de maíz.</p>
+
+
+                    </section>
+                    <section class="4u"><a href="#" class="image feature"><img src="images/Almendrap.png" alt=""
+                                width="368" height="180"></a>
+                        <p>Palanquetas con almendra</p>
+                        <p>Ingredientes: Almendra, Cacahuate, Ajonjolí, Amaranto, Azúcar de caña y Glucosa de maíz.</p>
+
+
+                    </section>
+                    <section class="4u"><a href="#" class="image feature"><img src="images/Nuezp.png" alt="" width="368"
+                                height="180"></a>
+                        <p>Palanquetas con nuez</p>
+                        <p>Ingredientes: Nuez, ajonjolí, amaranto, azúcar de caña y miel de maíz.</p>
+                    </section>
+                </div>
+            </section>
+        </div>
+
+        <!-- Main -->
+        <div id="main" class="wrapper style1">
+            <section class="container">
+                <header class="major">
+                    <h2>Nulla luctus eleifend</h2>
+                    <span class="byline">Mauris vulputate dolor sit amet nibh</span>
+                </header>
+                <div class="row">
+
+                    <!-- Content -->
+                    <div class="6u">
+                        <section>
+                            <ul class="style">
+                                <li>
+                                    <span class="fa fa-wrench"></span>
+                                    <h3>Integer ultrices</h3>
+                                    <span>In posuere eleifend odio. Quisque semper augue mattis wisi. Maecenas ligula.
+                                        Pellentesque viverra vulputate enim.</span>
+                                </li>
+                                <li>
+                                    <span class="fa fa-cloud"></span>
+                                    <h3>Aliquam luctus</h3>
+                                    <span>Pellentesque viverra vulputate enim. Aliquam erat volutpat. Maecenas
+                                        condimentum enim tincidunt risus accumsan.</span>
+                                </li>
+                            </ul>
+                        </section>
+                    </div>
+                    <div class="6u">
+                        <section>
+                            <ul class="style">
+                                <li>
+                                    <span class="fa fa-cogs"></span>
+                                    <h3>Integer ultrices</h3>
+                                    <span>In posuere eleifend odio. Quisque semper augue mattis wisi. Maecenas ligula.
+                                        Pellentesque viverra vulputate enim.</span>
+                                </li>
+                                <li>
+                                    <span class="fa fa-leaf"></span>
+                                    <h3>Aliquam luctus</h3>
+                                    <span>Pellentesque viverra vulputate enim. Aliquam erat volutpat. Maecenas
+                                        condimentum enim tincidunt risus accumsan.</span>
+                                </li>
+                            </ul>
+                        </section>
+                    </div>
+
+                </div>
+            </section>
+        </div>
+
+        <!-- Footer -->
+        <div id="footer">
+            <div class="container">
+
+                <!-- Lists -->
+                <div class="row">
+                    <div class="8u">
+                        <section>
+                            <header class="major">
+                                <h2>Donec dictum metus</h2>
+                                <span class="byline">Quisque semper augue mattis wisi maecenas ligula</span>
+                            </header>
+                            <div class="row">
+                                <section class="6u">
+                                    <ul class="default">
+                                        <li><a href="#">Pellentesque elit non gravida blandit.</a></li>
+                                        <li><a href="#">Lorem ipsum dolor consectetuer elit.</a></li>
+                                        <li><a href="#">Phasellus nibh pellentesque congue.</a></li>
+                                        <li><a href="#">Cras vitae metus aliquam pharetra.</a></li>
+                                    </ul>
+                                </section>
+                                <section class="6u">
+                                    <ul class="default">
+                                        <li><a href="#">Pellentesque elit non gravida blandit.</a></li>
+                                        <li><a href="#">Lorem ipsum dolor consectetuer elit.</a></li>
+                                        <li><a href="#">Phasellus nibh pellentesque congue.</a></li>
+                                        <li><a href="#">Cras vitae metus aliquam pharetra.</a></li>
+                                    </ul>
+                                </section>
+                            </div>
+                        </section>
+                    </div>
+                    <div class="4u">
+                        <section>
+                            <header class="major">
+                                <h2>Donec dictum metus</h2>
+                                <span class="byline">Mattis wisi maecenas ligula</span>
+                            </header>
+                            <ul class="contact">
+                                <li>
+                                    <span class="address">Address</span>
+                                    <span>1234 Somewhere Road #4285 <br>Nashville, TN 00000</span>
+                                </li>
+                                <li>
+                                    <span class="mail">Mail</span>
+                                    <span><a href="#">someone@untitled.tld</a></span>
+                                </li>
+                                <li>
+                                    <span class="phone">Phone</span>
+                                    <span>(000) 000-0000</span>
+                                </li>
+                            </ul>
+                        </section>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="copyright">
+            Design by: <a href="https://templated.co/">TEMPLATED</a>
+        </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\homeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',homeController::class);
+Route::get('',homeController::class);
